b245fef28be371995144f0d2b9cf8a2f4685985b
Tarolewd <taro.lewdgmail.com>
2022-05-06 19:50:20 +0200

    push

--- second-encounter/script.js
+++ second-encounter/script.js
@@ -1,13 +1,21 @@
 /* global IntersectionObserver */
 const main = () => {
+  const videos = document.querySelectorAll('video')
   const checkEntry = entry => {
     if (entry.isIntersecting) {
       entry.target.play()
     } else {
-      entry.target.play().then(_ => entry.target.pause())
+      entry.target.play().then(() => entry.target.pause())
     }
   }
   const observer = new IntersectionObserver(entries => entries.forEach(checkEntry))
-  document.querySelectorAll('video').forEach(video => observer.observe(video))
+  const playPause = event => videos.forEach(video => video.play().then(() => {
+    video.pause()
+    observer.disconnect()
+    videos.forEach(video => observer.observe(video))
+  }))
+  window.addEventListener('click', playPause, { once: true })
+  window.addEventListener('touchstart', playPause, { once: true })
+  videos.forEach(video => observer.observe(video))
 }
 main()
