f4e1e8f3bf113a1a635646f7cc4417c16cd8892b
Tarolewd <68655640+tarolewd@users.noreply.github.com>
2023-05-18 23:13:57 +0200

    up

--- kaarchive/assets/home/random/tmp/generate_poster.ps1
+++
@@ -1,8 +0,0 @@
-# Settings
-$width=400
-$height=-2
-
-Get-ChildItem .\* -Include *.mp4, *.mov, *.mkv | 
-ForEach-Object {
-   & ffmpeg -i $_.FullName -vframes 1 -vf "scale=$($width):$($height):flags=lanczos" ".\poster\$($_.BaseName).webp"
-}
--- kaarchive/assets/home/scale_videos.ps1
+++
@@ -1,8 +0,0 @@
-# Settings
-$width=100
-$height=100
-
-Get-ChildItem .\* -Include *.mp4, *.mov, *.mkv |
-ForEach-Object {
-   & ffmpeg -i $_.FullName -vf "scale=$($width):$($height):flags=lanczos" ".\$($_.BaseName)_scaled$($_.Extension)"
-}
